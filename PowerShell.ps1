7f62a974cae6384dbd81f41f32fc236b58512563
yumiduka <yumiduka@outlook.com>
2019-03-16 19:05:16 +0900

    install.ps1からprofile関係を分離。

---
+++ PowerShell.ps1
@@ -0,0 +1,27 @@
+﻿# 変数
+
+$ProfileRoot = Split-Path $PROFILE
+$InitialRoot = Join-Path $PSScriptRoot 'PowerShell'
+
+# Profile用ディレクトリ、シンボリックリンク作成
+
+(
+  @{ ItemType = 'Directory'; Path = $ProfileRoot; Force = $true },
+  @{ ItemType = 'SymbolicLink'; Path = $env:PSModulePath.Split(';')[0]; Value = (Join-Path $InitialRoot 'Modules') },
+  @{ ItemType = 'SymbolicLink'; Path = $PROFILE.CurrentUserAllHosts; Value = (Join-Path $InitialRoot 'profile.ps1') },
+  @{ ItemType = 'SymbolicLink'; Path = $PROFILE; Value = (Join-Path $InitialRoot (Split-Path -Leaf $PROFILE)) },
+  @{ ItemType = 'SymbolicLink'; Path = '~\.fontlist'; Value = (Join-Path $PSScriptRoot '.fontlist') }
+) | % {
+  ## 配布先が既に存在する場合、次のエントリへ進む
+  if ( Test-Path $_.Path ) {
+    return
+  }
+
+  ## 配布元が存在しない場合、次のエントリへ進む(ISE用)
+  if ( ! (Test-Path $_.Value) ) {
+    return
+  }
+
+  ## アイテムを作成
+  ni @_
+}
--- install.ps1
+++ install.ps1
@@ -1,30 +1,6 @@
-﻿# 変数
+﻿# profile配布
 
-$ProfileRoot = Split-Path $PROFILE
-$InitialRoot = Join-Path $PSScriptRoot 'PowerShell'
-
-# Profile用ディレクトリ、シンボリックリンク作成
-
-(
-  @{ ItemType = 'Directory'; Path = $ProfileRoot; Force = $true },
-  @{ ItemType = 'SymbolicLink'; Path = $env:PSModulePath.Split(';')[0]; Value = (Join-Path $InitialRoot 'Modules') },
-  @{ ItemType = 'SymbolicLink'; Path = $PROFILE.CurrentUserAllHosts; Value = (Join-Path $InitialRoot 'profile.ps1') },
-  @{ ItemType = 'SymbolicLink'; Path = $PROFILE; Value = (Join-Path $InitialRoot (Split-Path -Leaf $PROFILE)) },
-  @{ ItemType = 'SymbolicLink'; Path = '~\.fontlist'; Value = (Join-Path $PSScriptRoot '.fontlist') }
-) | % {
-  ## 配布先が既に存在する場合、次のエントリへ進む
-  if ( Test-Path $_.Path ) {
-    return
-  }
-
-  ## 配布元が存在しない場合、次のエントリへ進む(ISE用)
-  if ( ! (Test-Path $_.Value) ) {
-    return
-  }
-
-  ## アイテムを作成
-  ni @_
-}
+& (Join-Path $PSScriptRoot 'PowerShell.ps1')
 
 # エクスプローラーの3Dオブジェクト削除
 
